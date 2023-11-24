cc101d15e1b97072acd7a939cf93e5469e6c9195
unknown <rimijetishi87@gmail.com>
2023-11-24 08:20:17 +0100

    Pick a Place

---
+++ 01-starting-project/src/components/DeleteConfirmation.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import ProgressBar from "./ProgressBar";
+import './DeleteConfirmation.css'
+const TIMER = 3000
+export default function DeleteConfirmation({ onConfirm, onCancel }) {
+  
+  
+
+  useEffect(() => {
+    console.log('setTimer')
+    const timer = setTimeout(() => {
+      onConfirm()
+  
+    }, TIMER);
+
+    return () => {
+      clearTimeout(timer)
+    }
+
+  }, [])
+
+  
+  return (
+    <div id="delete-confirmation">
+      <h2>Are you sure?</h2>
+      <p>Do you really want to remove this place?</p>
+      <div id="confirmation-actions">
+        <button onClick={onCancel} className="button-text">
+          No
+        </button>
+        <button onClick={onConfirm} className="button">
+          Yes
+        </button>
+      </div>
+      <ProgressBar timer={TIMER}/>
+    </div>
+  );
+}
